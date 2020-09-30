286b6f197c6b4fa65cdc932ecac0e07127606e4d
Khishigbayar <khishig25@gmail.com>
2020-09-30 19:50:36 +0800

    шоо шиддэг болно

--- app.js
+++ app.js
@@ -8,19 +8,28 @@ tohlohchiin tsugluulsan onoo
 var activePlayer = 1;
 var scores = [0, 0];
 var roundScore = 0;
-var dice = Math.round(Math.random() * 5) + 1;
+var diceNumber = Math.round(Math.random() * 5) + 1;
 
 //document.querySelector("#score-0").textContent = dice;
 //document.querySelector("#score-1").innerHTML = "<em>dice</em>";
 
 //програм эхлэхэд бэлдэх
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+//document.querySelector("#score-0").textContent = 0;
+// ene
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.round(Math.random() * 5) + 1;
+  console.log(diceNumber);
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
+diceDom.style.display = "block";
 
-console.log(dice);
+console.log(diceNumber);
 
 //<div class="player-score" id="score-0">43</div>
